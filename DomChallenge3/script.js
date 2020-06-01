8b2f65f11d68948b93269f2ce0fb122682bf585b
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-01 19:32:26 -0400

    first commit

---
+++ DomChallenge3/script.js
@@ -0,0 +1,32 @@
+let addRows;
+let addColumns;
+let removeRows;
+let removeColumns;
+let rows;
+let columns;
+let buttonSubmit = document.getElementById("button-submit");
+
+
+buttonSubmit.addEventListener("click", function(event){
+addRows = document.getElementById("addRows").value;
+addColumns = document.getElementById("addColumns").value;
+removeRows = document.getElementById("removeRows").value;
+removeColumns = document.getElementById("removeColumn").value;
+});
+
+
+let buttonColorUncolored = document.getElementById("colorUncolored");
+buttonColorUncolored.addEventListener("click", function(event){
+
+});
+
+
+let buttonColorAll = document.getElementById("colorAll");
+buttonColorAll.addEventListener("click", function(event){
+
+});
+
+let buttonClearAll = document.getElementById("clearAll");
+buttonClearAll.addEventListener("click", function(event){
+
+});
